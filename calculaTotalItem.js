e5470bd632e4d03c29ed312e71eeea9cfebae9d6
Marmitt <lmateus.marmitt@gmail.com>
2017-05-02 20:49:05 -0300

    Teste funcionando

--- calculaTotalItem.js
+++ calculaTotalItem.js
@@ -1,12 +1,12 @@
 const calculaTotalItem = function(codigo, quantidade) {
   const produtos = [
    {id: 1, nome: 'Banana', valorUnitario: 2},
-   {id: 2, nome: 'Maça', valorUnitario: 5}
-   {id: 3, nome: 'Pera', valorUnitario: 6}
-   {id: 4, nome: 'Morango', valorUnitario: 1}
+   {id: 2, nome: 'Maça', valorUnitario: 5},
+   {id: 3, nome: 'Pera', valorUnitario: 6},
+   {id: 4, nome: 'Morango', valorUnitario: 1},
   ];
 
-  return {produto:'Maça' valor: 15 }
+  return {produto:'Maça', valor: 15 };
 
 }
 module.exports = { calculaTotalItem };
--- test/calculaTotalItem.spec.js
+++ test/calculaTotalItem.spec.js
@@ -3,11 +3,11 @@ const lab = exports.lab = Lab.script();
 
 const { expect } = require('code');
 
-const { calculaTotalItem } = require('./calculaTotalItem');
+const { calculaTotalItem } = require('../calculaTotalItem');
 
 lab.experiment('Soma de 3 maçãs', () => {
   lab.test('O valor será R$15', (done) => {
-  expect(calculaTotalItem(2,3)).to.equal({produto:'Maçã' valor: 15 });
+  expect(calculaTotalItem(2,3)).to.equal({produto:'Maça', valor: 15 });
   done();
   });
 });
