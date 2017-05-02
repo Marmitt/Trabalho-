c068a78253d19162828a2542709a219f7d3c851c
Marmitt <lmateus.marmitt@gmail.com>
2017-05-02 19:50:32 -0300

    Inclusão do teste novo

--- Trabalho.js
+++ Trabalho.js
@@ -1,16 +1,15 @@
 function CalculaTotalItem(produtos = [], opcoes = {}){
 let r = { resultado: [] };
 const produtos = (opcoes = {}) => {
-  produtos = [];
+  lista = [];
   quantidade;
 {[
   {id: 1, nome: 'Banana', valorUnitario: 2},
-  {id: 2, nome: 'Maça', valorUnitario: 4.5}
+  {id: 2, nome: 'Maça', valorUnitario: 5}
   {id: 3, nome: 'Pera', valorUnitario: 6}
-  {id: 4, nome: 'Morango', valorUnitario: 1.5}
+  {id: 4, nome: 'Morango', valorUnitario: 1}
 ]}
 
-return  {
+return
 
-}
 }
--- Trabalho.spec.js
+++ Trabalho.spec.js
@@ -3,15 +3,11 @@ const lab = exports.lab = Lab.script();
 
 const { expect } = require('code');
 
-const { CalculaTotalItem } = require('../CalculaTotalItem');
+const { CalculaTotalItem } = require('../Trabalho.js');
 
-  lab.experiment('Quanto custa 5 maçãs', () => {
-  lab.test('Devera custar 22.5', (done) => {
-  const produtos = ['Maçã', 'Banana', 'Pera', 'Morango'];
-  const retorno = { resultado:[
-    {Produto: 'Maçã', unidade: 4}
-  };
-  expect(CalculaTotalItem(produtos,{Maçã})).to.equal({resultado: [22.5]});
+lab.experiment('Soma de 3 maçãs', () => {
+  lab.test('O valor será R$15', (done) => {
+  expect(CalculaTotalItem(2,3)).to.equal({produto:'Maçã' valor: 15 });
   done();
   });
 });
